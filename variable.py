f365fe21349817088ae3f7962bebc8662eb5df4d
Николай <dhyeheydddjeujd>
2024-10-01 17:51:47 +0300

    variable

---
+++ variable.py
@@ -0,0 +1,9 @@
+The_number_of_completed_DZ = 12
+print(The_number_of_completed_DZ)
+Number_of_hours_spent = 1.5
+print(Number_of_hours_spent)
+Course_name = 'Python'
+print(Course_name)
+Time_for_one_task = 1.5 / 12
+print(Time_for_one_task)
+print(Course_name, 'всего часов',The_number_of_completed_DZ,'затраченно часов',Number_of_hours_spent, 'среднее время выполнения' ,Time_for_one_task, 'часа')
